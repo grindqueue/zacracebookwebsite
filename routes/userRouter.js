5d9e3d6f404d4e87400b00949e2554a422139f44
Ope <omolewa.opeyemi@lmu.edu.ng>
2025-09-16 13:25:26 +0100

    Add purchased API

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,8 @@ const isAuthenticated = require('../middlewares/isAuth');
 const { getPurchasedBooks, getTransactionHistory, streamBookFile } = require('../admincontroller/adminForms.js')
 
 
-userRouter.get('/transactions/:userId', getTransactionHistory)
-userRouter.get('/stream-book/:productId', streamBookFile);
+userRouter.get('/transactions/:userId', isAuthenticated, getTransactionHistory)
+userRouter.get('/stream-book/:productId', isAuthenticated, streamBookFile);
+userRouter.get('/purchased-books/:userId', getPurchasedBooks);
 
 module.exports = userRouter;
